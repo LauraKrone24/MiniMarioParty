1b8c88e85014853608ce19409c171425f6e04b28
Laura Krone <laura.krone@web.de>
2023-05-07 13:16:53 +0200

    Created Feld Klasse

---
+++ src/main/java/com/example/minimarioparty/Feld.java
@@ -0,0 +1,24 @@
+package com.example.minimarioparty;
+
+public class Feld {
+    private final int number;
+    private final int x;
+    private final int y;
+    public Feld(int number,int x, int y ){
+        this.number = number;
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
