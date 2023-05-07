21ad0e7bcfd6edcd04822d740307c84569608886
Laura Krone <laura.krone@web.de>
2023-05-07 16:58:49 +0200

    Hauptgame fxml erster entwurf

--- src/main/java/com/example/minimarioparty/Hauptgame.java
+++ src/main/java/com/example/minimarioparty/Hauptgame.java
@@ -5,10 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Hauptgame extends Application {
+    private Spieler[] spieler = new Spieler[2];
+    private Spieler aktuellerSpieler;
     private ArrayList<Feld> felder = new ArrayList<Feld>();
     @Override
     public void start(Stage stage) throws IOException {
@@ -17,10 +20,19 @@ public class Hauptgame extends Application {
         stage.setTitle("Mini Mario Party");
         stage.setScene(scene);
         stage.show();
+        String spielername = JOptionPane.showInputDialog ("Gib deinen Namen ein: ");
+        spieler[0] = new Spieler(spielername,false);
+        spieler[1] =  new Spieler("Computer",true);
+        aktuellerSpieler = chooseStartspieler();
+        System.out.println(aktuellerSpieler.getName());
     }
 
     public void setFelder(){
 
+    };
+    public Spieler chooseStartspieler(){
+
+        return spieler[(int)Math.random()*2];
     };
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.minimarioparty {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.minimarioparty to javafx.fxml;
