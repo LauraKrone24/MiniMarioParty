4133ae46ee36c5ba28b11629d868fc768d48c0b9
sungrh <wi22077@lehre.dhbw-stuttgart.de>
2023-05-07 16:49:01 +0200

    tictactoe test

--- src/main/java/com/example/minimarioparty/Aktionsfeld.java
+++ src/main/java/com/example/minimarioparty/Aktionsfeld.java
@@ -3,7 +3,7 @@ package com.example.minimarioparty;
 import java.util.List;
 
 public class Aktionsfeld extends Feld{
-    private List<Minispiel>  minispielListe = new List<Minispiel>();
+    private List<Minispiel>  minispielListe;
     public Aktionsfeld(int number, int x, int y) {
         super(number, x, y);
         // hier muss auch noch die Minispielliste initialisert werden
