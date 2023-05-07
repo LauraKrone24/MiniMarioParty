eef44de10d04366da1865da34f5752ff4e4e52a9
LucaFeulner <Luca_feulner@outlook.de>
2023-05-07 15:47:44 +0200

    Spielerklasse

--- src/main/java/com/example/minimarioparty/Spieler.java
+++ src/main/java/com/example/minimarioparty/Spieler.java
@@ -6,19 +6,27 @@ public class Spieler {
     private String name;
     private boolean computer;
     private List<Wuerfel> wuerfelList;
+    private Feld position;
     //hier Grafik einfügen
 
     public Spieler(String name, boolean computer){
         this.name = name;
         this.computer = computer;
+        wuerfelList.add(new Wuerfel("Normaler Wuefel",1,6));
 
     }
 
-
+    // getter Methoden
     public String getName() {
         return name;
     }
 
+    public Feld getPosition() {
+        return position;
+    }
+
+
+    //Methoden
     public int wuerfeln(){
         int ergebnis = 0;
 
@@ -26,6 +34,15 @@ public class Spieler {
     }
 
     public void bewegeSpieler(){
+
+        int ergebnis = this.position.getNumber() + wuerfeln();
+        if (ergebnis<99){
+            //  this.position= felder[ergebnis];
+            System.out.println("Bewegt zu "+ergebnis);
+        }else {
+            System.out.println("Du hast gewonnen");
+        }
+
 
 
     }
