71f67849789bc4f8b06c71377be1373eb234d337
Leyla54 <brendlerleyla@gmail.com>
2023-05-07 15:37:23 +0200

    Würfelklasse update

--- src/main/java/com/example/minimarioparty/Wuerfel.java
+++ src/main/java/com/example/minimarioparty/Wuerfel.java
@@ -1,14 +1,30 @@
 package com.example.minimarioparty;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Wuerfel {
+    private String name;
 
-    private String normalerWuerfel;
-    private String leichterGewonnenerWuerfel;
-    private String schwererGewonnenerWuerfel;
-    ArrayList<Integer> Werfel = new ArrayList<>();
+    int start;
+    int end;
+
+    public Wuerfel(String name, int start, int end){
+        this.name = name;
+        this.start= start;
+        this.end= end;
+
+    }
+
+    public int wuerfeln(){
+        int wurf;
+        int differenz;
+        differenz= end - start;
+        wurf= (int)(Math.random()*differenz+start);
+        return wurf;
+
+    }
+
+
 
 
 
