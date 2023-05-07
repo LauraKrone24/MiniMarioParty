074b9c1705dea729cd44d500a40e6188056538f7
sungrh <wi22077@lehre.dhbw-stuttgart.de>
2023-05-07 16:45:31 +0200

    tictactoe test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.minimarioparty {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens com.example.minimarioparty to javafx.fxml;
     exports com.example.minimarioparty;
+    exports com.example.minimarioparty.Minispiele.Test;
+    opens com.example.minimarioparty.Minispiele.Test to javafx.fxml;
+
 }
