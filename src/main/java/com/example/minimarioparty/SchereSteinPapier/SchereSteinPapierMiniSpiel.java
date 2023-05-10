97a4c35951affb33a90dc29ed978197898051899
LucaFeulner <Luca_feulner@outlook.de>
2023-05-10 13:17:48 +0200

    Hauptgame setFelder fertig

---
+++ src/main/java/com/example/minimarioparty/SchereSteinPapier/SchereSteinPapierMiniSpiel.java
@@ -0,0 +1,14 @@
+package com.example.minimarioparty.SchereSteinPapier;
+
+import com.example.minimarioparty.Minispiel;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SchereSteinPapierMiniSpiel extends Minispiel {
+    @Override
+    public SchereSteinPapierMiniSpiel (Stage stage) throws IOException {
+        super.start(stage);
+
+    }
+}
